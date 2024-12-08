1eb9627e1ce88d0b5ca129c2d0e667fd9bdda613
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2024-12-08 21:20:44 +0100

    Strict threads - not working

--- EmscriptenSettings.cmake
+++ EmscriptenSettings.cmake
@@ -281,6 +281,7 @@ function(emscripten_settings)
       "-pthread"
       "-sUSE_PTHREADS=1"
       "-sSHARED_MEMORY=1"
+      "-sPTHREAD_POOL_SIZE_STRICT" # Not working
       "-sPTHREAD_POOL_SIZE=${ARGS_THREAD_POOL_SIZE}")
   endif()
 
