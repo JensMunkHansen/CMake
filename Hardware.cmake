250e64fd1705e862cafc5d1c6f9696283dad4268
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2024-12-09 19:40:06 +0100

    ..

--- Hardware.cmake
+++ Hardware.cmake
@@ -1,34 +1,32 @@
-# Include CMake module for processor count
-include(ProcessorCount)
+# Define a reusable function for getting processor count and other hardware info
+function(sps_get_processor_count OUTPUT_VAR MAX_CONCURRENCY_VAR)
+  if (UNIX)
+    # Execute a system command to get the processor count
+    execute_process(COMMAND nproc OUTPUT_VARIABLE PROCESSOR_COUNT OUTPUT_STRIP_TRAILING_WHITESPACE)
 
-# Get the processor count
-ProcessorCount(PROCESSOR_COUNT)
-
-# Check if processor count was successfully retrieved
-if(PROCESSOR_COUNT EQUAL 0)
-    message(WARNING "Processor count could not be determined. Defaulting to 1.")
-    set(PROCESSOR_COUNT 1)
-else()
+    # Fallback if the result is empty
+    if (PROCESSOR_COUNT STREQUAL "")
+      message(WARNING "Processor count could not be determined. Defaulting to 1.")
+      set(PROCESSOR_COUNT 1)
+    endif()
+    
     message(STATUS "Detected ${PROCESSOR_COUNT} processors.")
-endif()
+  else()
+    include(ProcessorCount)
 
-# Use PROCESSOR_COUNT in your build process
-set(MAX_CONCURRENCY ${PROCESSOR_COUNT})
+    # Get the processor count
+    ProcessorCount(PROCESSOR_COUNT)
 
-if (0)
-  include(ProcessorCount)
-  
-  # Execute a system command to get the processor count
-  execute_process(COMMAND nproc OUTPUT_VARIABLE PROCESSOR_COUNT OUTPUT_STRIP_TRAILING_WHITESPACE)
-  
-  # Fallback if the result is empty
-  if(PROCESSOR_COUNT STREQUAL "")
-    message(WARNING "Processor count could not be determined. Defaulting to 1.")
-    set(PROCESSOR_COUNT 1)
+    # Check if processor count was successfully retrieved
+    if (PROCESSOR_COUNT EQUAL 0)
+      message(WARNING "Processor count could not be determined. Defaulting to 1.")
+      set(PROCESSOR_COUNT 1)
+    else()
+      message(STATUS "Detected ${PROCESSOR_COUNT} processors.")
+    endif()
   endif()
-  
-  message(STATUS "Detected ${PROCESSOR_COUNT} processors.")
-  
-  # Use PROCESSOR_COUNT in your build process
-  set(MAX_CONCURRENCY ${PROCESSOR_COUNT})
-endif()
+
+  # Assign outputs to the provided variables
+  set(${OUTPUT_VAR} ${PROCESSOR_COUNT} PARENT_SCOPE)
+  set(${MAX_CONCURRENCY_VAR} ${PROCESSOR_COUNT} PARENT_SCOPE)
+endfunction()
