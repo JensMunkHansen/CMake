a03d9c4ffa418d265735300dbd972e7cb4194921
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2025-02-22 23:58:37 +0100

    bench

--- spsBenchmark.cmake
+++ spsBenchmark.cmake
@@ -1,7 +1,7 @@
 find_package(benchmark QUIET)
 
 # If not found, use FetchContent
-if (NOT benchmark_FOUND OR EMSCRIPTEN)
+if (1)#NOT benchmark_FOUND OR EMSCRIPTEN)
   include(FetchContent)
   message(STATUS "Google Benchmark not found, using FetchContent...")
 
@@ -10,6 +10,9 @@ if (NOT benchmark_FOUND OR EMSCRIPTEN)
     set(CMAKE_C_FLAGS "-matomics -mbulk-memory")
   endif()
 
+  set(FETCHCONTENT_BASE_DIR "${CMAKE_BINARY_DIR}/../cmake-dependencies")
+  set(FETCHCONTENT_FULLY_DISCONNECTED ON) # Prevent auto-deleting
+  
   FetchContent_Declare(benchmark
     GIT_REPOSITORY https://github.com/google/benchmark.git
     GIT_TAG v1.8.3
@@ -20,7 +23,7 @@ if (NOT benchmark_FOUND OR EMSCRIPTEN)
 
   # Disable tests to speed up build
   set(BENCHMARK_ENABLE_TESTING OFF CACHE BOOL "Disable benchmark tests" FORCE)
-
+  set(BENCHMARK_ENABLE_GTEST_TESTS OFF CACHE BOOL "Disable benchmark gtests" FORCE)
   # Fetch and make it available
   FetchContent_MakeAvailable(benchmark)
 else()
