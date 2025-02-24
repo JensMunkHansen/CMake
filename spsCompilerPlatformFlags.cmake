8884111f1e0b3cddd156549244d8f59155fb0ba4
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2025-02-24 22:12:24 +0100

    Now clang is faster

--- spsCompilerPlatformFlags.cmake
+++ spsCompilerPlatformFlags.cmake
@@ -7,12 +7,23 @@ if (TARGET build)
     target_compile_options(build
       INTERFACE
       # GNU flags for Release
-      $<$<AND:$<CXX_COMPILER_ID:GNU>,$<CONFIG:Release>>:-O3 -march=native -mtune=native -flto -fuse-linker-plugin -ftree-vectorize -fopt-info-vec>
+      $<$<AND:$<CXX_COMPILER_ID:GNU>,$<CONFIG:Release>>:-O3 -march=native -mtune=native -funroll-loops -ffast-math -flto -fuse-linker-plugin -ftree-vectorize -fopt-info-vec>
       # GNU flags for Debug
       $<$<AND:$<CXX_COMPILER_ID:GNU>,$<CONFIG:Debug>>:-O0 -g>
 
       # Clang flags for Release
-      $<$<AND:$<CXX_COMPILER_ID:Clang>,$<CONFIG:Release>>:-O3 -march=native -Rpass=loop-vectorize -Rpass-missed=loop-vectorize>
+      $<$<AND:$<CXX_COMPILER_ID:Clang>,$<CONFIG:Release>>:
+      -O3 -march=native -mtune=native -flto=full -funroll-loops
+      -ffast-math -ffp-contract=fast -fassociative-math -freciprocal-math  # Matches GCC's -ffast-math  
+      -Rpass=loop-vectorize -Rpass-missed=loop-vectorize -Rpass-analysis=loop-vectorize>
+      # -fwhole-program-vtables -fprefetch-loop-arrays
+      # -mllvm -enable-loop-flatten
+      # -mllvm -enable-epilogue-vectorization
+      # -mllvm -enable-slp-vectorization
+
+      # TODO: Profile-Guided Optimization (PGO)
+      #       Binary Optimization Layout Tool (BOLT) on the final binary
+      
       # Clang flags for Debug
       $<$<AND:$<CXX_COMPILER_ID:Clang>,$<CONFIG:Debug>>:-O0 -g>
       
