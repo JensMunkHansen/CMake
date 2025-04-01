d2b3f5b206c6b3daed34eb0bc6ab939afbaa0b9f
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2025-04-01 22:11:11 +0200

    ..

--- spsCompilerPlatformSupport.cmake
+++ spsCompilerPlatformSupport.cmake
@@ -1,5 +1,12 @@
 include(CheckCXXSourceCompiles)
 
+# Save the original flags
+set(_old_flags "${CMAKE_REQUIRED_FLAGS}")
+
+# Check for specific C++20 features
+
+set(CMAKE_REQUIRED_FLAGS "-std=c++20")
+
 check_cxx_source_compiles("
   #include <memory>
   #include <atomic>
@@ -14,10 +21,13 @@ check_cxx_source_compiles("
     return 0;
   }
 " SPS_ATOMIC_SHARED_PTR)
-set(${PROJECT_NAME}_ATOMIC_SHARED_PTR ${SPS_ATOMIC_SHARED_PTR} PARENT_SCOPE)
+set(${PROJECT_NAME}_ATOMIC_SHARED_PTR ${SPS_ATOMIC_SHARED_PTR})
 
 if(${PROJECT_NAME}_ATOMIC_SHARED_PTR)
   message(STATUS "Compiler supports std::atomic<std::shared_ptr<T>>")
 else()
   message(WARNING "Compiler does NOT support std::atomic<std::shared_ptr<T>>")
 endif()
+
+# Restore original flags
+set(CMAKE_REQUIRED_FLAGS "${_old_flags}")
