f39b681f941ddd82ae18a1dcbe374c7081f2792e
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2025-04-11 18:44:55 +0200

    fmt check

--- spsCompilerPlatformSupport.cmake
+++ spsCompilerPlatformSupport.cmake
@@ -4,7 +4,6 @@ include(CheckCXXSourceCompiles)
 set(_old_flags "${CMAKE_REQUIRED_FLAGS}")
 
 # Check for specific C++20 features
-
 set(CMAKE_REQUIRED_FLAGS "-std=c++20")
 
 check_cxx_source_compiles("
@@ -29,5 +28,35 @@ else()
   message(WARNING "Compiler does NOT support std::atomic<std::shared_ptr<T>>")
 endif()
 
+set(_old_libraries "${CMAKE_REQUIRED_LIBRARIES}")
+
+# Check for specific C++20 features
+set(CMAKE_REQUIRED_LIBRARIES "stdc++")
+
+check_cxx_source_compiles("
+  #include <format>
+  #include <string>
+  int main() {
+    auto s = std::format(\"{}\", 42);
+    return 0;
+  }
+" HAS_STD_FORMAT)
+
+set(${PROJECT_NAME}_STD_FORMAT} ${HAS_STD_FORMAT})
+
+if(HAS_STD_FORMAT)
+  message(STATUS "Compiler supports std::format")
+else()
+  message(WARNING "Compiler does NOT support std::format")
+endif()
+
+if (HAS_STD_FORMAT)
+  target_compile_definitions(build INTERFACE USE_STD_FORMAT=1)
+else()
+  find_package(fmt REQUIRED)
+  target_link_libraries(build INTERFACE fmt::fmt)
+endif()
+
 # Restore original flags
 set(CMAKE_REQUIRED_FLAGS "${_old_flags}")
+set(CMAKE_REQUIRED_LIBRARIES "${_old_libraries}")
