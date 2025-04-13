0390593f154a8c783555d08db62ff6a4408cc8cc
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2025-04-13 20:36:11 +0200

    ..

--- spsFindBindingSources.cmake
+++ spsFindBindingSources.cmake
@@ -1,49 +1,59 @@
 function(find_embinding_modules top_folder out_var)
-    file(GLOB_RECURSE embinding_files
-        RELATIVE "${top_folder}"
-        "${top_folder}/**/vtk*Embinding.cxx"
-    )
+  set(options VERBOSE)
+  cmake_parse_arguments(FEM "${options}" "" "" ${ARGN})
+  file(GLOB_RECURSE embinding_files
+    RELATIVE "${top_folder}"
+    "${top_folder}/**/vtk*Embinding.cxx"
+  )
 
-    set(result "")
+  set(result "")
 
-    foreach(rel_path IN LISTS embinding_files)
-        # Clear values before loop iteration
-        unset(binding_name)
-        unset(module_name)
-        unset(abs_path)
+  foreach(rel_path IN LISTS embinding_files)
+    # Clear values before loop iteration
+    unset(binding_name)
+    unset(module_name)
+    unset(abs_path)
 
-        get_filename_component(dir_path "${rel_path}" DIRECTORY)
-        get_filename_component(file_name "${rel_path}" NAME)
+    get_filename_component(dir_path "${rel_path}" DIRECTORY)
+    get_filename_component(file_name "${rel_path}" NAME)
 
-        message(STATUS "Checking: ${rel_path}")
-        message(STATUS "  Dir path: ${dir_path}")
-        message(STATUS "  File name: ${file_name}")
+    if(FEM_VERBOSE)
+      message(STATUS "Checking: ${rel_path}")
+      message(STATUS "  Dir path: ${dir_path}")
+      message(STATUS "  File name: ${file_name}")
+    endif()
+    string(REGEX MATCH "^vtk(.+)Embinding\\.cxx$" _ "${file_name}")
+    if(NOT CMAKE_MATCH_1)
+      if(FEM_VERBOSE)
+        message(WARNING "  --> File did not match vtkXXXEmbinding.cxx")
+      endif()
+      continue()
+    endif()
 
-        string(REGEX MATCH "^vtk(.+)Embinding\\.cxx$" _ "${file_name}")
-        if(NOT CMAKE_MATCH_1)
-            message(WARNING "  --> File did not match vtkXXXEmbinding.cxx")
-            continue()
-        endif()
+    set(binding_name "${CMAKE_MATCH_1}")
+    if(FEM_VERBOSE)
+      message(STATUS "  --> Matched binding name: ${binding_name}")
+    endif()
 
-        set(binding_name "${CMAKE_MATCH_1}")
-        message(STATUS "  --> Matched binding name: ${binding_name}")
+    if(dir_path STREQUAL "")
+      if(FEM_VERBOSE)
+        message(WARNING "  --> Skipping: no subdirectory")
+      endif()
+      continue()
+    endif()
 
-        if(dir_path STREQUAL "")
-            message(WARNING "  --> Skipping: no subdirectory")
-            continue()
-        endif()
+    string(REPLACE "/" "" module_name "${dir_path}")
+    # string(REPLACE "/" "_" module_name "${dir_path}")	
+    get_filename_component(abs_path "${top_folder}/${rel_path}" ABSOLUTE)
 
-        string(REPLACE "/" "" module_name "${dir_path}")
-	# string(REPLACE "/" "_" module_name "${dir_path}")	
-        get_filename_component(abs_path "${top_folder}/${rel_path}" ABSOLUTE)
-
-        message(STATUS "  --> Module name: ${module_name}")
-        message(STATUS "  --> ABSOLUTE PATH: ${abs_path}")
-
-        list(APPEND result "${binding_name}" "${module_name}")
-    endforeach()
-    
-    set(${out_var} "${result}" PARENT_SCOPE)
+    if(FEM_VERBOSE)
+      message(STATUS "  --> Module name: ${module_name}")
+      message(STATUS "  --> ABSOLUTE PATH: ${abs_path}")
+    endif()
+    list(APPEND result "${binding_name}" "${module_name}")
+  endforeach()
+  
+  set(${out_var} "${result}" PARENT_SCOPE)
 endfunction()
 
 
@@ -100,4 +110,27 @@ function(class_module_lookup embindings class_name out_module_var)
     # If not found
     message(WARNING "Class ${class_name} not found in embinding info")
     set(${out_module_var} "" PARENT_SCOPE)
+endfunction()
+
+function(list_modules embindings out_var)
+    set(info "${${embindings}}")
+    set(modules "")
+
+    list(LENGTH info count)
+    math(EXPR pair_count "${count} / 2")
+
+    foreach(i RANGE 0 ${pair_count})
+        math(EXPR name_index "${i} * 2")
+        math(EXPR module_index "${name_index} + 1")
+
+        if(module_index GREATER_EQUAL count)
+            break()
+        endif()
+
+        list(GET info ${module_index} module_name)
+        list(APPEND modules "${module_name}")
+    endforeach()
+
+    list(REMOVE_DUPLICATES modules)
+    set(${out_var} "${modules}" PARENT_SCOPE)
 endfunction()
