1e3333458e4851ccc83536f9ee0ec6b35eb38a30
Jens Munk Hansen <Jens.Hansen@3shape.com>
2025-06-25 10:20:49 +0200

    fixed compile-commands

--- spsCompileCommands.cmake
+++ spsCompileCommands.cmake
@@ -1,5 +1,10 @@
 if(CMAKE_EXPORT_COMPILE_COMMANDS)
-  execute_process(COMMAND ${CMAKE_COMMAND} -E copy_if_different
-    "${CMAKE_BINARY_DIR}/compile_commands.json"
-    "${CMAKE_SOURCE_DIR}/compile_commands.json")
+  add_custom_target(copy_compile_commands ALL
+    COMMAND ${CMAKE_COMMAND} -E copy_if_different
+      "${${PROJECT_NAME}_BINARY_DIR}/compile_commands.json"
+      "${${PROJECT_NAME}_SOURCE_DIR}/compile_commands.json"
+    DEPENDS "${${PROJECT_NAME}_BINARY_DIR}/compile_commands.json"
+    COMMENT "Copying compile_commands.json to source dir"
+    VERBATIM
+  )
 endif()
