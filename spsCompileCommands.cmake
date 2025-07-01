9ac11a78eacb3cbe04c723d1c7717acd559802b4
Jens Munk Hansen <jens.munk.hansen@gmail.com>
2025-07-01 19:01:37 +0200

    updated compile commands

--- spsCompileCommands.cmake
+++ spsCompileCommands.cmake
@@ -1,10 +1,14 @@
 if(CMAKE_EXPORT_COMPILE_COMMANDS)
+  add_custom_command(
+    OUTPUT "${${PROJECT_NAME}_SOURCE_DIR}/compile_commands.json"
+    COMMAND ${CMAKE_COMMAND} -E copy_if_different
+            "${${PROJECT_NAME}_BINARY_DIR}/compile_commands.json"
+            "${${PROJECT_NAME}_SOURCE_DIR}/compile_commands.json"
+    DEPENDS "${${PROJECT_NAME}_BINARY_DIR}/compile_commands.json"
+    COMMENT "Copying compile_commands.json"
+  )
+  
   add_custom_target(copy_compile_commands ALL
-    COMMAND ${CMAKE_COMMAND}
-      -DSOURCE="${${PROJECT_NAME}_BINARY_DIR}/compile_commands.json"
-      -DDESTINATION_DIR="${${PROJECT_NAME}_SOURCE_DIR}/compile_commands.json"
-      -P ${CMAKE_CURRENT_LIST_DIR}/CopyCompileCommands.cmake
-    COMMENT "Copying compile_commands.json to source dir"
-    VERBATIM
+    DEPENDS "${${PROJECT_NAME}_BINARY_DIR}/compile_commands.json"
   )
 endif()
